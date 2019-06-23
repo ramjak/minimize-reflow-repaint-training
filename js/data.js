f093c87b2c555c4d5c3076795b8729d4ed4540eb
ramjak <rama.jakaria@gmail.com>
2019-06-23 21:23:01 +0700

    init

---
+++ js/data.js
@@ -0,0 +1,102 @@
+const data =  [
+  {
+    "No": 1,
+    "Name": "Profile #1",
+    "Avatar": "/img/001-backpack.png"
+  },
+  {
+    "No": 2,
+    "Name": "Profile #2",
+    "Avatar": "/img/002-van.png"
+  },
+  {
+    "No": 3,
+    "Name": "Profile #3",
+    "Avatar": "/img/003-signpost.png"
+  },
+  {
+    "No": 4,
+    "Name": "Profile #4",
+    "Avatar": "/img/004-camera.png"
+  },
+  {
+    "No": 5,
+    "Name": "Profile #5",
+    "Avatar": "/img/005-ticket.png"
+  },
+  {
+    "No": 6,
+    "Name": "Profile #6",
+    "Avatar": "/img/006-tent.png"
+  },
+  {
+    "No": 7,
+    "Name": "Profile #7",
+    "Avatar": "/img/007-passport.png"
+  },
+  {
+    "No": 8,
+    "Name": "Profile #8",
+    "Avatar": "/img/008-hot air balloon.png"
+  },
+  {
+    "No": 9,
+    "Name": "Profile #9",
+    "Avatar": "/img/009-luggage.png"
+  },
+  {
+    "No": 10,
+    "Name": "Profile #10",
+    "Avatar": "/img/010-favorite.png"
+  },
+  {
+    "No": 11,
+    "Name": "Profile #11",
+    "Avatar": "/img/011-baggage.png"
+  },
+  {
+    "No": 12,
+    "Name": "Profile #12",
+    "Avatar": "/img/012-car.png"
+  },
+  {
+    "No": 13,
+    "Name": "Profile #13",
+    "Avatar": "/img/013-image.png"
+  },
+  {
+    "No": 14,
+    "Name": "Profile #14",
+    "Avatar": "/img/014-glasses.png"
+  },
+  {
+    "No": 15,
+    "Name": "Profile #15",
+    "Avatar": "/img/015-snorkel.png"
+  },
+  {
+    "No": 16,
+    "Name": "Profile #16",
+    "Avatar": "/img/016-exchange.png"
+  },
+  {
+    "No": 17,
+    "Name": "Profile #17",
+    "Avatar": "/img/017-globe.png"
+  },
+  {
+    "No": 18,
+    "Name": "Profile #18",
+    "Avatar": "/img/018-tag.png"
+  },
+  {
+    "No": 19,
+    "Name": "Profile #19",
+    "Avatar": "/img/019-popsicle.png"
+  },
+  {
+    "No": 20,
+    "Name": "Profile #20",
+    "Avatar": "/img/020-map.png"
+  }
+]
---
+++ js/main.js
@@ -0,0 +1,115 @@
+function shuffle(arr) {
+  for (let i = arr.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [arr[i], arr[j]] = [arr[j], arr[i]];
+  }
+  return arr;
+}
+
+function createElementOneByOne(rootElement, data) {
+  rootElement.innerHTML = '';
+  for (let i = 0; i < data.length; i++) {
+    const newTr = document.createElement('tr');
+
+    const newNoTd = document.createElement('td');
+    newNoTd.innerText = data[i].No;
+    newTr.appendChild(newNoTd);
+
+    const newNameTd = document.createElement('td');
+    newNameTd.innerText = data[i].Name;
+    newTr.appendChild(newNameTd);
+
+    const newAvatarTd = document.createElement('td');
+    const newAvatarImg = new Image(50, 50);
+    newAvatarImg.src = data[i].Avatar;
+    newAvatarTd.appendChild(newAvatarImg);
+    newTr.appendChild(newAvatarTd);
+
+    rootElement.appendChild(newTr);
+  }
+}
+
+function createElementAllAtOnce(rootElement, data) {
+  rootElement.innerHTML = '';
+  const wrapperElement = document.createDocumentFragment();
+  for (let i = 0; i < data.length; i++) {
+    const newTr = document.createElement('tr');
+
+    const newNoTd = document.createElement('td');
+    newNoTd.innerText = data[i].No;
+    newTr.appendChild(newNoTd);
+
+    const newNameTd = document.createElement('td');
+    newNameTd.innerText = data[i].Name;
+    newTr.appendChild(newNameTd);
+
+    const newAvatarTd = document.createElement('td');
+    const newAvatarImg = new Image(50, 50);
+    newAvatarImg.src = data[i].Avatar;
+    newAvatarTd.appendChild(newAvatarImg);
+    newTr.appendChild(newAvatarTd);
+
+    wrapperElement.appendChild(newTr);
+  }
+  rootElement.appendChild(wrapperElement);
+}
+
+function replaceText(rootElement, data, prevData) {
+  for (let i = 0; i < data.length; i++) {
+    if (data[i] !== prevData[i]) {
+      const children = rootElement.children[i].children;
+      children[0].innerText = data[i].No;
+      children[1].innerText = data[i].Name;
+      children[2].firstChild.src = data[i].Avatar;
+    }
+  }
+}
+
+function getOperationTime(cb) {
+  const start = performance.now();
+  cb();
+  const end = performance.now();
+
+  return end - start;
+}
+
+window.addEventListener('DOMContentLoaded', () => {
+  const tableElement = document.getElementById('table');
+  const tableBody = document.getElementById('data-table-body');
+  const time = document.getElementById('time');
+  const reButton = document.getElementById('re');
+  const hreButton = document.getElementById('hre');
+  const rtButton = document.getElementById('rt');
+  const doEmptyButton = document.getElementById('doEmpty');
+  const doFixedLayoutInput = document.getElementById('do-fixed-layout');
+  let prevData = data;
+
+  time.innerText = getOperationTime(() => createElementOneByOne(tableBody, data));
+
+  doEmptyButton.addEventListener('click', () => {
+    tableBody.innerHTML = '';
+  });
+
+  reButton.addEventListener('click', () => {
+    const newData = shuffle([...data]);
+    time.innerText = getOperationTime(() => createElementOneByOne(tableBody, newData));
+    prevData = newData;
+  });
+
+
+  hreButton.addEventListener('click', () => {
+    const newData = shuffle([...data]);
+    time.innerText = getOperationTime(() => createElementAllAtOnce(tableBody, newData));
+    prevData = newData;
+  });
+
+  rtButton.addEventListener('click', () => {
+    const newData = shuffle([...data]);
+    time.innerText = getOperationTime(() => replaceText(tableBody, newData, prevData));
+    prevData = newData;
+  });
+
+  doFixedLayoutInput.addEventListener('change', (e) => {
+    tableElement.style.tableLayout = e.target.checked ? 'fixed' : 'auto';
+  });
+});
